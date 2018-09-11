dbd202156bdaad995cea31c2f8c59f8de09c40b1
Oscar O'Brien <oscar.obrien@me.com>
2018-09-11 15:18:00 +0100

    Added framework for inner app components. (Linked router to components, added tabs in dashboard etc)

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,10 @@ import Router from 'vue-router'
 import TheDashboard from '@/views/TheDashboard'
 import TheLogIn from '@/views/LogIn/TheLogIn'
 import TheSignUp from '@/views/LogIn/TheSignUp'
+import TheInfo from '@/views/InnerApp/TheInfo'
+import ThePractice from '@/views/InnerApp/ThePractice'
+import TheStatistics from '@/views/InnerApp/TheStatistics'
+import TheTest from '@/views/InnerApp/TheTest'
 
 Vue.use(Router)
 
@@ -21,6 +25,26 @@ export default new Router({
           path: '/signup',
           name: 'TheSignUp',
           component: TheSignUp
+        },
+        {
+          path: '/info',
+          name: 'TheInfo',
+          component: TheInfo
+        },
+        {
+          path: '/practice',
+          name: 'ThePractice',
+          component: ThePractice
+        },
+        {
+          path: '/statistics',
+          name: 'TheStatistics',
+          component: TheStatistics
+        },
+        {
+          path: '/test',
+          name: 'TheTest',
+          component: TheTest
         }
       ]
     }
--- src/views/TheDashboard.vue
+++ src/views/TheDashboard.vue
@@ -27,12 +27,56 @@
             </template>
             <template v-if="hasLoggedIn">
 
+                <li>
+                    <router-link
+                      :to="{name: 'TheInfo'}"
+                      v-bind:class="['header-list-item', {link_part_active: selectedOption === 0}]"
+                      v-on:click.native="selectedOption = 0"
+                      tag='li'>
+                        Info
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link
+                      :to="{name: 'ThePractice'}"
+                      v-bind:class="['header-list-item', {link_part_active: selectedOption === 1}]"
+                      v-on:click.native="selectedOption = 1"
+                      tag='li'>
+                        Practice
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link
+                      :to="{name: 'TheTest'}"
+                      v-bind:class="['header-list-item', {link_part_active: selectedOption === 2}]"
+                      v-on:click.native="selectedOption = 2"
+                      tag='li'>
+                        Test
+                    </router-link>
+                </li>
+
+                <li>
+                    <router-link
+                      :to="{name: 'TheStatistics'}"
+                      v-bind:class="['header-list-item', {link_part_active: selectedOption === 3}]"
+                      v-on:click.native="selectedOption = 3"
+                      tag='li'>
+                        Statistics
+                    </router-link>
+                </li>
+
             </template>
         </ul>
 
         <div id='page-container'>
             <TheLogIn @loginSuccess="onLogin" v-if="selectedOption === 0 && !hasLoggedIn"/>
             <TheSignUp @signupSuccess="onLogin" v-if="selectedOption === 1 && !hasLoggedIn"/>
+            <TheInfo v-if="selectedOption === 0 && hasLoggedIn"/>
+            <ThePractice v-if="selectedOption === 1 && hasLoggedIn"/>
+            <TheTest v-if="selectedOption === 2 && hasLoggedIn"/>
+            <TheStatistics v-if="selectedOption === 3 && hasLoggedIn"/>
         </div>
 
     </div>
@@ -41,12 +85,20 @@
 <script>
 import TheLogIn from '@/views/LogIn/TheLogIn'
 import TheSignUp from '@/views/LogIn/TheSignUp'
+import TheInfo from '@/views/InnerApp/TheInfo'
+import ThePractice from '@/views/InnerApp/ThePractice'
+import TheStatistics from '@/views/InnerApp/TheStatistics'
+import TheTest from '@/views/InnerApp/TheTest'
 
 export default {
   name: 'TheDashboard',
   components: {
     TheLogIn,
-    TheSignUp
+    TheSignUp,
+    TheInfo,
+    ThePractice,
+    TheStatistics,
+    TheTest
   },
 
   data () {
@@ -61,6 +113,7 @@ export default {
     onLogin: function (username) {
       this.username = username
       this.hasLoggedIn = true
+      this.SelectedOption = 0
       console.log('Successfully accessed internal app with user: ' + username)
     }
   }
