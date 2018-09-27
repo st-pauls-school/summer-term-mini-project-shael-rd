fcb0db91d418e9d1a49cccefc189dfe063c3ac9a
Oscar O'Brien <oscar.obrien@me.com>
2018-09-27 11:55:52 +0100

    Am able to draw in a canvas, on mouseover! Tried to find a strategy to combine the mosuedown and mouseover events - it's not easy.

--- src/components/WritingCanvas.vue
+++ src/components/WritingCanvas.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <canvas width="300" height="150" style="border:1px solid #BBB;" v-on: v-drawToCanvas></canvas>
+        <canvas id="DrawingArea" width="300" height="150" style="border:1px solid #BBB;" v-on:mousemove="mouseIsDown ? mouseOnCanvas : 0"></canvas>
+        <h1>{{curLocation}}</h1>
     </div>
 </template>
 
@@ -10,19 +11,45 @@ export default {
 
   data () {
     return {
-      selectedOption: 0
+      points: [],
+      pointsCounter: 0,
+      mouseIsDown: false,
+      curLocation: ''
     }
   },
 
   methods: {
-    canvasMousedOver: function 
+    mouseOnCanvas: function (event) {
+      var canvas = document.getElementById('DrawingArea')
+      var ctx = canvas.getContext('2d')
+      var rect = canvas.getBoundingClientRect()
+      var mouseX = Math.floor(event.clientX - rect.left)
+      var mouseY = Math.floor(event.clientY - rect.top)
 
+      this.points.push({x: mouseX, y: mouseY})
+      if (this.pointsCounter > 0) {
+        ctx.moveTo(this.points[this.pointsCounter - 1].x, this.points[this.pointsCounter - 1].y)
+        ctx.lineTo(this.points[this.pointsCounter].x, this.points[this.pointsCounter].y)
+        ctx.stroke()
+      }
+      this.pointsCounter++
+
+      this.curLocation = ''
+      this.curLocation += 'X: '
+      this.curLocation += event.clientX - rect.left
+      this.curLocation += ' Y: '
+      this.curLocation += event.clientY - rect.top
     }
   },
 
-  directives () {
-    drawToCanvas: {
-
+  events: {
+    mouseIsPressed: function () {
+      console.log('oof1')
+      this.mouseIsDown = true
+    },
+    mouseIsNotPressed: function () {
+      console.log('oof2')
+      this.mouseIsDown = false
     }
   }
 }
