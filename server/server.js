f403571c78f994142f5eafb35fb19810a81ce1bb
Oscar O'Brien <oscar.obrien@me.com>
2019-01-21 13:19:56 +0000

    Added sql injection protection in server file through mysql.format method, which escapes all input. Also started cleaning up code.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
+/**
+ * 
+ */
 import {constants} from '../src/constants.js'
 const express = require('express')
+const mysql = require('mysql')
 const url = require('url')
 const server = express()
 const router = express.Router()
@@ -27,7 +31,6 @@ router.use((request, response, next) => {
 })
 
 var getCon = function () {
-  var mysql = require('mysql')
   var con = mysql.createConnection({
     host: constants.mySQLHost,
     user: constants.mySQLUser,
@@ -39,14 +42,11 @@ var getCon = function () {
 
 router.post('/signup', (request, response) => {
   console.log('Signup request received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
+  let parameters = url.parse(request.url, true).query
 
-  var con = getCon()
+  let con = getCon()
 
-  var queryString = 'SELECT * FROM UserList WHERE username="'
-  queryString += parameters.q
-  queryString += '"'
+  let queryString = mysql.format('SELECT * FROM UserList WHERE username = ?', [parameters.q])
   console.log(queryString)
 
   con.connect(function (err) {
@@ -70,16 +70,11 @@ router.post('/signup', (request, response) => {
 
 router.post('/login', (request, response) => {
   console.log('Login request received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
+  let parameters = url.parse(request.url, true).query
 
-  var con = getCon()
+  let con = getCon()
 
-  var queryString = 'SELECT * FROM UserList WHERE username="'
-  queryString += parameters.q
-  queryString += '" AND password="'
-  queryString += parameters.r
-  queryString += '"'
+  let queryString = mysql.format('SELECT * FROM UserList WHERE username = ? AND password = ?', [parameters.q, parameters.r])
   console.log(queryString)
 
   con.connect(function (err) {
@@ -104,22 +99,17 @@ router.post('/login', (request, response) => {
 
 router.post('/signupNewUser', (request, response) => {
   console.log('\nRequest to add user received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
+  let parameters = url.parse(request.url, true).query
 
-  var mkdirp = require('mkdirp')
+  let mkdirp = require('mkdirp')
   mkdirp('./userIMGs/' + parameters.user, function(err) {
     if (err) console.log('Failed to create folder for user' + parameters.user)
     else console.log('Folder creation successful.')
   })
 
-  var con = getCon()
+  let con = getCon()
 
-  var queryString = 'INSERT INTO UserList (username, password) VALUES ("'
-  queryString += parameters.user
-  queryString += '", "'
-  queryString += parameters.pass
-  queryString += '")'
+  let queryString = mysql.format('INSERT INTO UserList (username, password) VALUES (?, ?)', [parameters.user, parameters.pass])
   console.log('Query String: ' + queryString)
 
   con.connect(function (err) {
@@ -141,13 +131,10 @@ router.post('/signupNewUser', (request, response) => {
 
 router.post('/randomWord', (request, response) => {
   console.log('Request to get random word received.')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
-  var con = getCon()
+  let con = getCon()
 
-  var queryString = 'SELECT w.* FROM Words AS w JOIN '
-  queryString += '(SELECT ROUND(RAND() * (SELECT MAX(id) FROM Words)) AS id) AS x '
-  queryString += 'WHERE w.id >= x.id LIMIT 1;'
+  
+  let queryString = mysql.format('SELECT w.* FROM Words AS w JOIN (SELECT ROUND(RAND() * (SELECT MAX(id) FROM Words)) AS id) AS x WHERE w.id >= x.id LIMIT 1')
 
   con.connect(function (err) {
     if (err) {
@@ -170,11 +157,10 @@ router.post('/randomWord', (request, response) => {
 
 router.post('/getUserId', (request, response) => {
   console.log('Request to grab userId by username received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
-  var con = getCon()
+  let parameters = url.parse(request.url, true).query
+  let con = getCon()
 
-  var queryString = 'SELECT userid FROM userlist WHERE username="' + parameters.username + '"'
+  let queryString = mysql.format('Select userid FROM userlist WHERE username = ?', [parameters.username])
   console.log(queryString)
 
   con.connect(function (err) {
@@ -197,17 +183,12 @@ router.post('/getUserId', (request, response) => {
 
 router.post('/submitScore', (request, response) => {
   console.log('Request to submit user score received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
-  var con = getCon()
+  let parameters = url.parse(request.url, true).query
+  let con = getCon()
 
-  var queryString = 'INSERT INTO TestResults (testtype, userid, score, time, date, accuracy) VALUES ("'
-  queryString += parameters.testtype + '", "'
-  queryString += parameters.userid + '", '
-  queryString += parameters.score + ', '
-  queryString += parameters.time + ', '
-  queryString += 'CURRENT_TIMESTAMP, '
-  queryString += parameters.accuracy + ')'
+  
+  let queryString = mysql.format('INSERT INTO TestResults (testtype, userid, score, time, date, accuracy) VALUES(?, ?, ?, ?, ?, ?)',
+   [parameters.testtype, parameters.userid, parameters.score, parameters.time, 'CURRENT_TIMESTAMP', parameters.accuracy])
   console.log('Query String: ' + queryString)
 
   con.connect(function (err) {
@@ -231,15 +212,16 @@ router.post('/submitScore', (request, response) => {
 
 router.post('/getLastWordid', (request, response) => {
   console.log('Request to fetch last autoincrement wordid received')
-  var con = getCon()
+  let con = getCon()
 
+  let queryString = mysql.format('SELECT Max(wordid) as id FROM WordScore')
   con.connect(function (err) {
     if (err) {
       console.log('Unable to connect to database.\n')
       return
     }
     console.log('Connected to MySQL database.')
-    con.query('SELECT Max(wordid) as id FROM WordScore', function (err, result) {
+    con.query(queryString, function (err, result) {
       if (err) {
         console.log('Unable to get wordid from wordScores in database.\n')
         response.json('no')
@@ -253,13 +235,12 @@ router.post('/getLastWordid', (request, response) => {
 
 router.post('/submitWordData', (request, response) => {
   console.log('Request to submit word data received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
-  var con = getCon()
+  let parameters = url.parse(request.url, true).query
+  let con = getCon()
 
   parameters.drawingSurface = parameters.drawingSurface.replace(/ /g, '+')
-  var fs = require('fs')
-  var path = './userIMGs/' + parameters.username + '/' + parameters.wordid + '_' + parameters.word + '.txt'
+  let fs = require('fs')
+  let path = './userIMGs/' + parameters.username + '/' + parameters.wordid + '_' + parameters.word + '.txt'
   fs.writeFile(path, parameters.drawingSurface, function (err, data) {
     if (err) {
       console.log('Failed to create file: ', err)
@@ -268,11 +249,8 @@ router.post('/submitWordData', (request, response) => {
     }
   })
 
-  var queryString = 'INSERT INTO WordScore (date, word, score, userid) VALUES ('
-  queryString += 'CURRENT_TIMESTAMP, '
-  queryString += '"' + parameters.word + '", '
-  queryString += parameters.score + ', '
-  queryString += parameters.userid + ')'
+  let queryString = mysql.format('INSERT INTO WordScore (date, word, score, userid) VALIES (?, ?, ?, ?)',
+  ['CURRENT_TIMESTAMP', parameters.word, parameters.score, parameters.userid])
   console.log('Query String: ' + queryString)
 
   con.connect(function (err) {
@@ -295,13 +273,11 @@ router.post('/submitWordData', (request, response) => {
 
 router.post('/getHighscore', (request, response) => {
   console.log('Request to grab highscore received')
-  var urlParts = url.parse(request.url, true)
-  var parameters = urlParts.query
-  var con = getCon()
+  let parameters = url.parse(request.url, true).query
+  let con = getCon()
 
-  var queryString = 'SELECT MAX(score) AS highscore FROM TestResults WHERE '
-  queryString += 'userid=' + parameters.userid
-  queryString += ' AND testtype="' + parameters.testtype + '"'
+  let queryString = mysql.format('SELECT MAX(score) AS highscore FROM TestResults WHERE userid = ? AND testtype = ?',
+  [parameters.userid, parameters.testtype])
   console.log('Query string: ', queryString)
 
   con.connect(function (err) {
@@ -325,8 +301,14 @@ router.post('/getHighscore', (request, response) => {
 router.post('/getRecentResults', (request, response) => {
   console.log('Request to grab recent scores received')
   let parameters = url.parse(request.url, true).query
-  var con = getCon()
+  let con = getCon()
 
+  let rawQueryString = 'SELECT * FROM (SELECT * FROM ? WHERE userid = ?'
+  if(parameters.table === 'TestResults') rawQueryString += ' AND testtype = ' + con.escape(parameters.testtype)
+  rawQueryString += ' ORDER BY date DESC LIMIT ?) sub ORDER BY date ASC'
+  let queryString = mysql.format(rawQueryString, [parameters.table, parameters.userid, parameters.number])
+
+  /*
   let queryString = 'SELECT * FROM ('
   queryString += 'SELECT * FROM ' + parameters.table
   queryString += ' WHERE userid=' + parameters.userid + ' '
@@ -334,6 +316,7 @@ router.post('/getRecentResults', (request, response) => {
   queryString += 'ORDER BY date DESC LIMIT ' + parameters.number + ') sub '
   queryString += 'ORDER BY date ASC'
   console.log('Query String: ', queryString)
+  */
 
   con.connect(function (err) {
     if (err) {
@@ -356,11 +339,10 @@ router.post('/getRecentResults', (request, response) => {
 router.post('/getAvgScores', (request, response) => {
   console.log('Request to return average scores received')
   let parameters = url.parse(request.url, true).query
-  var con = getCon()
+  let con = getCon()
 
-  let queryString = 'SELECT AVG(time) as avg_time, AVG(score) as avg_score, AVG(accuracy) as avg_accuracy FROM testresults WHERE testType="'
-  queryString += parameters.type + '"'
-
+  let queryString = mysql.format('SELECT AVG(time) as avg_time, AVG(score) as avg_score, AVG(accuracy) as avg_accuracy' +
+' FROM testresults WHERE testType = ?', [parameters.type])
   console.log(queryString)
 
   con.connect(function (err) {
@@ -385,14 +367,12 @@ router.post('/getImageUrl', (request, response) => {
   console.log('Request to return image url received')
   let parameters = url.parse(request.url, true).query
 
-
-  var fs = require('fs')
-  var files = fs.readdirSync('./userIMGs/' + parameters.username)
+  let fs = require('fs')
+  let files = fs.readdirSync('./userIMGs/' + parameters.username)
   for (var f of files) {
-    console.log(f)
     if (f.includes(parameters.id.toString())) {
-      var returnURL = fs.readFileSync('./userIMGs/' + parameters.username + '/' + f).toString()
-      var word = f.replace(parameters.id.toString() + '_', '')
+      let returnURL = fs.readFileSync('./userIMGs/' + parameters.username + '/' + f).toString()
+      let word = f.replace(parameters.id.toString() + '_', '')
       word = word.replace('.txt', '')
 
       response.json({url: returnURL, word: word})
